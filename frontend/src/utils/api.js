cce87e8c5cbf1dce6bb62fbdb98bdc5bdef44bab
Artem Ivanov <aie426205@gmail.com>
2021-04-23 14:09:24 +0300

    fix: base-url fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://api.ivart.students.nomoredomains.icu';
+const BASE_URL = 'https://api.ivart.students.nomoredomains.icu';
 // const BASE_URL = 'http://localhost:3000';
 
 class Api {
